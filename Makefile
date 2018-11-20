e95f6613dc9efb2a7a9b0c261da428d4567ba83f
Toon Claes <toon@iotcl.com>
2018-11-20 20:26:36 +0100

    First working decent version of org-based

--- Makefile
+++ Makefile
@@ -1,3 +1,24 @@
+publish_dir := public
+timestamps_dir := .timestamps
+orgs := $(wildcard *.org)
+emacs_pkgs := org
+
+publish_el := elisp/publish.el
+
+^el = $(filter %.el,$^)
+EMACS.funcall = emacs --batch --no-init-file $(addprefix --load ,$(^el)) --funcall
+
+all: publish
+
+publish: $(publish_el) $(orgs)
+	$(EMACS.funcall) rw-publish-all
+
+clean:
+	rm -rf $(publish_dir)
+	rm -rf $(timestamps_dir)
+
+
+
 mds:=$(wildcard _posts/*.md)
 orgs:=$(patsubst %.md,%.org,$(mds))
 
---
+++ elisp/publish.el
@@ -0,0 +1,116 @@
+;;; publish --- Publish org files to GitLab Pages
+
+;;; Commentary:
+
+;; This file takes care of exporting org files to the public directory.
+;; Images and such are also exported without any processing.
+
+;;; Code:
+
+(require 'package)
+(package-initialize)
+(unless package-archive-contents
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-refresh-contents))
+(dolist (pkg '(org-plus-contrib htmlize))
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
+(require 'org)
+(require 'ox-publish)
+
+(defvar rw-root
+  (let ((dir default-directory))
+    (while (not (file-directory-p (expand-file-name ".git" dir)))
+      (setq dir (expand-file-name ".." dir)))
+    dir)
+  "The root directory of this project.")
+
+(defvar rw--layouts-directory
+  (expand-file-name "layouts" rw-root)
+  "Directory where layouts are found.")
+
+(defun rw--pre/postamble-format (type)
+  "Return the content for the pre/postamble of TYPE."
+  `(("en" ,(with-temp-buffer
+             (insert-file-contents (expand-file-name (format "%s.html" type) rw--layouts-directory))
+             (buffer-string)))))
+
+(defvar rw--site-attachments
+  (regexp-opt '("jpg" "jpeg" "gif" "png" "svg"
+                "ico" "cur" "css" "js"
+                "eot" "woff" "woff2" "ttf"
+                "html" "pdf"))
+  "File types that are published as static files.")
+
+(defun rw/org-publish-sitemap (_title list)
+  "Default site map, as a string.
+LIST is an internal representation for the files to include, as
+returned by `org-list-to-lisp'.  PROJECT is the current project."
+  (concat (org-list-to-org list)))
+
+(defun rw/org-publish-sitemap-entry (entry style project)
+  "Format for sitemap ENTRY, as a string.
+ENTRY is a file name.  STYLE is the style of the sitemap.
+PROJECT is the current project."
+  (let ((creation-date (format-time-string "%Y-%m-%d" (org-publish-find-date entry project))))
+    (format "[[file:%s][%s]] /posted on %s/"
+	    entry
+	    (org-publish-find-title entry project)
+            creation-date)))
+
+(defvar rw--publish-project-alist
+      (list
+       (list "blog-posts"
+             :base-directory "posts"
+             :base-extension "org"
+             :recursive nil
+             :publishing-function 'org-html-publish-to-html
+             :publishing-directory "./public"
+             :html-head-include-default-style nil
+             :html-head-include-scripts nil
+             :html-htmlized-css-url "css/style.css"
+             :html-head-extra
+             "<link rel=\"icon\" type=\"image/x-icon\" href=\"/favicon.ico\" />"
+             :html-preamble-format (rw--pre/postamble-format 'preamble)
+             :html-postamble t
+             :html-postamble-format (rw--pre/postamble-format 'postamble)
+             :auto-sitemap t
+             :sitemap-filename "index.org"
+             ;; :sitemap-title "rw-r--r-- | writepermission.com"
+             :sitemap-style 'list
+             ;; :sitemap-function 'rw/org-publish-sitemap
+             :sitemap-format-entry 'rw/org-publish-sitemap-entry)
+       (list "blog-static"
+             :base-directory "."
+             :exclude (regexp-opt '("public/"))
+             :base-extension rw--site-attachments
+             :publishing-directory "./public"
+             :publishing-function 'org-publish-attachment
+             :recursive t)
+       (list "site"
+             :components '("blog-posts"))
+       ))
+
+(defun rw-publish-all ()
+  "Publish the blog to HTML."
+  (interactive)
+  (let ((org-publish-project-alist       rw--publish-project-alist)
+        (org-publish-timestamp-directory "./.timestamps/")
+        (org-export-with-section-numbers nil)
+        (org-export-with-smart-quotes    t)
+        (org-export-with-toc             nil)
+        (org-html-divs '((preamble  "header" "top")
+                         (content   "main"   "content")
+                         (postamble "footer" "postamble")))
+        (org-html-container-element         "section")
+        (org-html-metadata-timestamp-format "%Y-%m-%d")
+        (org-html-checkbox-type             'html)
+        (org-html-html5-fancy               t)
+        (org-html-validation-link           nil)
+        (org-html-doctype                   "html5")
+        (org-html-htmlize-output-type       'css))
+    (org-publish-all)))
+
+;;; publish.el ends here
